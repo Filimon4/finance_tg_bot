eb12effcbb0c12d036a4b35d16bb34095322bcb6
FILIMON <f.rykov@bk.ru>
2025-03-18 22:15:02 +0300

    changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// import * as fs from "fs";
+import * as fs from "fs";
 
 import path from 'path';
 
@@ -10,18 +10,18 @@ const resolvePath = (p: string) => path.resolve(__dirname, p);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   port: 443,
-  //   host: "0.0.0.0",
-  //   hmr: {
-  //       host: 'tg-mini-app.local',
-  //       port: 443,
-  //   },
-  //   https: {
-  //     key: fs.readFileSync('./.cert/localhost-key.pem'),
-  //     cert: fs.readFileSync('./.cert/localhost.pem'),
-  //   },
-  // },
+  server: {
+    port: 443,
+    host: "0.0.0.0",
+    hmr: {
+        host: 'tg-mini-app.local',
+        port: 443,
+    },
+    https: {
+      key: fs.readFileSync('./.cert/localhost-key.pem'),
+      cert: fs.readFileSync('./.cert/localhost.pem'),
+    },
+  },
   resolve: {
     alias: {
         '@components': resolvePath('src/components'),
