f8e20dd3394499a2b10919aa6cd5dc2ae3b856de
qqwtxxg <shindzikatx@yandex.ru>
2025-03-20 20:18:12 +0300

    git commmit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// import * as fs from "fs";
+import * as fs from "fs";
 
 import path from 'path';
 
 const resolvePath = (p: string) => path.resolve(__dirname, p);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   port: 443,
-  //   host: "0.0.0.0",
-  //   hmr: {
-  //       host: 'tg-mini-app.local',
-  //       port: 443,
-  //   },
-  //   https: {
-  //     key: fs.readFileSync('./.cert/localhost-key.pem'),
-  //     cert: fs.readFileSync('./.cert/localhost.pem'),
-  //   },
-  // },
+  server: {
+    port: 443,
+    host: "0.0.0.0",
+    hmr: {
+        host: 'tg-mini-app.local',
+        port: 443,
+    },
+    https: {
+      key: fs.readFileSync('./.cert/localhost-key.pem'),
+      cert: fs.readFileSync('./.cert/localhost.pem'),
+    },
+  },
   resolve: {
     alias: {
         '@components': resolvePath('src/components'),
@@ -34,4 +33,4 @@ export default defineConfig({
 },
 })
 
-// Все одинаковые, просты мыслят по разному (С) Максим Бильчук
+// Все одинаковые, просто мыслят по разному (С) Максим Бильчук
